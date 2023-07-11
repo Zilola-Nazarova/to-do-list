8fb444a3266386d364d71b222cdec79a2e4bf545
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-12 03:02:23 +0500

    Load styles, icon, write the array of tasks, provide the name of the to-do list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import _ from 'lodash';
 import './style.css';
+import Drag from './drag-handle-minor-svgrepo-com.svg';
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack!'], ' ');
-  element.classList.add('hello');
-  return element;
-}
+const listName = "Today's To Do";
+const tasksArray = [
+  {
+    description: 'Read an article on Webpack',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'Create an index.js file',
+    completed: true,
+    index: 3,
+  },
+  {
+    description: 'Setup the Webpack',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Create an index.html file',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Add styles to the project',
+    completed: false,
+    index: 5,
+  },
+  {
+    description: 'Display the list',
+    completed: true,
+    index: 4,
+  },
+]
 
-document.body.appendChild(component());
