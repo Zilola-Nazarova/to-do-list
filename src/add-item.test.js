7e6ce3f4593f56a7bfecbd9127229d0267a8c256
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-19 19:25:12 +0500

    Create test file, import add-item function, provide a plan of what to test

---
+++ src/add-item.test.js
@@ -0,0 +1,41 @@
+import addTask from './add-item.js';
+
+describe('add to empty array', () => {
+  test('should create a new object with correct properties', () => {
+    // arrange
+
+    // act
+
+    // assert
+    
+  });
+
+  test('should add exactly one <li> element to the list in the DOM', () => {
+    // arrange
+
+    // act
+
+    // assert
+    
+  });
+});
+
+describe('add to existing array', () => {
+  test('should append a new object with correct properties to the end of the array', () => {
+    // arrange
+
+    // act
+
+    // assert
+    
+  });
+
+  test('should add exactly one <li> element to the list in the DOM', () => {
+    // arrange
+
+    // act
+
+    // assert
+
+  });
+});
