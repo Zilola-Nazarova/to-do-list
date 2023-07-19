e0f5916abbbb07e9ed848204cb09f0c725d3b70f
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-19 22:30:20 +0500

    Write two more tests to check if the number of <li> elements decreases by 1 by deleteTask function

--- src/delete-item.test.js
+++ src/delete-item.test.js
@@ -3,7 +3,7 @@ import deleteTask from './delete-item.js';
 jest.mock('./render-list.js');
 
 describe('delete from array of objects', () => {
-  test('should delete and object at a given position', () => {
+  test('should delete the object at a given position', () => {
     // arrange
     const arrayOfTasks = [
       {
@@ -35,7 +35,7 @@ describe('delete from array of objects', () => {
     ]);
   });
 
-  test('edge case when position is 0, should delete first object and update index', () => {
+  test('edge case when position is 0, should delete the first object and update index', () => {
     // arrange
     const arrayOfTasks = [
       {
@@ -102,5 +102,65 @@ describe('delete from array of objects', () => {
         index: 2,
       },
     ]);
+  });
+});
+
+describe('delete one <li> element from DOM', () => {
+  test('should delete one <li> element at a given position', () => {
+    // arrange
+    const arrayOfTasks = [
+      {
+        description: 'Peer-to-peer review',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'Call a mentor',
+        completed: true,
+        index: 2,
+      },
+    ];
+    const position = 1;
+    document.body.innerHTML = '<div>'
+    + '  <input id="add-task">'
+    + '  <ul id="list">'
+    + '  <li></li>'
+    + '  <li></li>'
+    + '  </ul>'
+    + '</div>';
+    // act
+    deleteTask(arrayOfTasks, position);
+    // assert
+    const list = document.querySelectorAll('#list li');
+    expect(list).toHaveLength(1);
+  });
+
+  test('should leave the <li> elements when position is greater than the length of the array', () => {
+    // arrange
+    const arrayOfTasks = [
+      {
+        description: 'Peer-to-peer review',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'Call a mentor',
+        completed: true,
+        index: 2,
+      },
+    ];
+    const position = 2;
+    document.body.innerHTML = '<div>'
+    + '  <input id="add-task">'
+    + '  <ul id="list">'
+    + '  <li></li>'
+    + '  <li></li>'
+    + '  </ul>'
+    + '</div>';
+    // act
+    deleteTask(arrayOfTasks, position);
+    // assert
+    const list = document.querySelectorAll('#list li');
+    expect(list).toHaveLength(2);
   });
 });
