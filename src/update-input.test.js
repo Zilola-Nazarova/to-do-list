fcf9379c7a5e1d5ee265363cdc6fce7a15fe252b
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-20 19:26:54 +0500

    Write a second test for updateInput function to check the localStorage state

--- src/update-input.test.js
+++ src/update-input.test.js
@@ -1,7 +1,6 @@
 import updateInput from './update-input.js';
 
 describe('to update already inserted item to discription', () => {
-
   test('Should update discription', () => {
     // arrange
     const arryOfTasks = [
@@ -29,22 +28,57 @@ describe('to update already inserted item to discription', () => {
     + '  </ul>'
     + '</div>';
     const position = 0;
-    const allInputs = document.querySelectorAll('input')
-    const givenInputs = allInputs[position]
-    givenInputs.value = 'Go for a Walk'
+    const allInputs = document.querySelectorAll('input');
+    const givenInputs = allInputs[position];
+    givenInputs.value = 'Go for a Walk';
+
     // act
     updateInput(arryOfTasks, position, givenInputs);
+
     // assert
     expect(arryOfTasks[position].description).toBe('Go for a Walk');
   });
 
-  test('', () => {
+  test('should update localStorage', () => {
     // arrange
+    const arrayOfTasks = [
+      {
+        description: 'Peer-to-peer review',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'Call a mentor',
+        completed: true,
+        index: 2,
+      },
+    ];
+    const position = 0;
+    document.body.innerHTML = '<div>'
+    + '  <input id="add-task">'
+    + '  <ul id="list">'
+    + '  <li><input /></li>'
+    + '  </ul>'
+    + '</div>';
+    const givenInput = document.querySelector('input');
+    givenInput.value = 'Peer-to-peer review (2x)';
 
     // act
+    updateInput(arrayOfTasks, position, givenInput);
+    const result = JSON.parse(localStorage.getItem('To-Do List'));
 
     // assert
-    
+    expect(result).toMatchObject([
+      {
+        description: 'Peer-to-peer review (2x)',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'Call a mentor',
+        completed: true,
+        index: 2,
+      },
+    ]);
   });
-
-})
+});
