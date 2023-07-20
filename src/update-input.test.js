152a05fc15d57c58f03cda3b6788e0653e0df143
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-20 18:54:51 +0500

    Create files for new tests for updateInput and updateStatus function, import these functions to test files

---
+++ src/update-input.test.js
@@ -0,0 +1,23 @@
+import updateInput from './update-input.js';
+
+describe('', () => {
+
+  test('', () => {
+    // arrange
+
+    // act
+
+    // assert
+
+  });
+
+  test('', () => {
+    // arrange
+
+    // act
+
+    // assert
+    
+  });
+
+})
---
+++ src/update-status.test.js
@@ -0,0 +1,23 @@
+import updateStatus from "./update-status";
+
+describe('', () => {
+
+  test('', () => {
+    // arrange
+
+    // act
+
+    // assert
+
+  });
+
+  test('', () => {
+    // arrange
+
+    // act
+
+    // assert
+    
+  });
+
+})
